231cb0234fe9d0b8d6b5457df30cfd7558c8d6fe
nikosbosse <nikosbosse@gmail.com>
2024-05-16 15:54:01 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,12 +5,27 @@ xcode-select --install
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # ================================================
+# set up git
+git config --global user.name "nikosbosse"
+git config --global user.email "nikosbosse@gmail.com"
+# ================================================
+
+# ================================================
+# clone relevant repos
+mkdir github-synced
+cd github-synced
+git clone https://github.com/nikosbosse/obsidian && cd obsidian && git checkout mac && cd ..
+git clone https://github.com/nikosbosse/zotero-nikos
+# ================================================
+
+# ================================================
 # install relevant apps
 brew install --cask iterm2
 brew install --cask keepassx
 brew install --cask r
 brew install --cask rstudio
 brew install --cask arc 
+brew install --cask brave-browser
 brew install --cask dropbox
 brew install --cask google-drive
 brew install --cask obsidian
@@ -44,8 +59,13 @@ echo 'alias python=python3' >> ~/.zshrc
 echo 'alias pip=pip3' >> ~/.zshrc
 # ================================================
 
+# javascript
+brew install node
+brew install nvm
+
 # other to dos 
 #Enable Darkmode
 #Enable Snap to grid in Finder
 #Switch caps and Esc, switch fn and ctrl
-
+# make screenshots save to clipboard
+# update taskleiste to show F1, F2 by default
